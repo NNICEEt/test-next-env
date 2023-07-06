38680dbf072b9a6d77049999b7a0b95e8dcc9eef
teerapat nayapan <nice@jenosize.com>
2023-07-06 15:34:53 +0700

    test ga

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import { useEffect, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
-  const [display, setDisplay] = useState('')
+  const [display, setDisplay] = useState("");
 
   useEffect(() => {
-    setDisplay(process.env.NEXT_PUBLIC_DISPLAY_ENV)
-  }, [])
+    setDisplay(process.env.NEXT_PUBLIC_DISPLAY_ENV);
+  }, []);
 
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-CQEX0HL2X3`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CQEX0HL2X3');
+        `}
+      </Script>
       <div>{display}</div>
       <div>{process.env.NODE_ENV}</div>
     </>
