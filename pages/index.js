d86964bfb97a4346c6d10bdb05132d293aadd4a3
teerapat nayapan <nice@jenosize.com>
2023-12-07 15:55:03 +0700

    aa

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,19 @@ function Home() {
       <div>{process.env.NODE_ENV}</div>
       <button
         onClick={() => {
-          gtag("event", "home_button_click", {
-            event_category: "click",
-            event_label: "button click",
-            value: "double click",
+          // gtag("event", "home_button_click", {
+          //   event_category: "click",
+          //   event_label: "button click",
+          //   value: "double click",
+          // });
+
+          window.dataLayer.push({
+            event: "event",
+            eventProps: {
+              category: "tracking_event_2",
+              action: "click",
+              label: "click_event_test_2",
+            },
           });
         }}
       >
